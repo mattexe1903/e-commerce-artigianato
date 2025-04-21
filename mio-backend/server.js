41e9079630c73825bd4610f9e5e28044619779b2
mattexe <teomarelli2@gmail.com>
2025-04-21 17:06:27 +0200

    improvements

--- mio-backend/server.js
+++ mio-backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const pool = require('./db');
+const pool = require('../db');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('../routes/authRoute');
 app.use('/api', authRoutes);
 
 app.get('/', (req, res) => {
