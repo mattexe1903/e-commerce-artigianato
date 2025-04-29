2023d059e1013fdad060e8798fd00614cc7c8863
mattexe <teomarelli2@gmail.com>
2025-04-29 22:16:42 +0200

    new script witch new role and permisssion management

--- script2.sql
+++ script2.sql
@@ -1,31 +1,84 @@
-CREATE TABLE utenti (
+-- Script per la creazione del database e delle tabelle per un sistema di gestione di un e-commerce
+
+--Creazione della tabella per la gestione dei ruoli con i relativi inserimenti inziali.
+CREATE TABLE ruoli
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO ruoli
+    (nome)
+VALUES
+    ('admin'),
+    ('utente'),
+    ('artigiano');
+
+--Creazione della tabella per la gestione dei permessi con i relativi inserimenti inziali.
+CREATE TABLE permessi
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO permessi
+    (nome)
+VALUES
+    ('login'),
+    ('registrazione come venditore'),
+    ('profilo utente'),
+    ('profilo artigiano'),
+    ('dashboard admin'),
+    ('visualizza prodotti'),
+    ('aggiungi prodotto'),
+    ('modifica prodotto'),
+    ('elimina prodotto'),
+    ('visualizza ordini'),
+    ('gestisci ordini'),
+    ('visualizza carrello'),
+    ('gestisci carrello'),
+    ('visualizza preferiti'),
+    ('gestisci preferiti');
+
+--Creazione della tabella per la gestione dei permessi associati ai ruoli.
+CREATE TABLE ruoli_permessi
+(
+    idPermesso INTEGER REFERENCES permessi(id),
+    idRuolo INTEGER REFERENCES ruoli(id),
+    permission_type CHAR,
+    PRIMARY KEY (idRuolo, idPermesso)
+);
+
+-- TO DO BETTER:
+INSERT INTO ruoli_permessi
+    (idPermesso, idRuolo, permission_type)
+values
+    (1, 1, 'W'),
+    (1, 2, 'W'),
+    (1, 3, 'W');
+
+--Creazione della tabella per la gestione degli utenti con i relativi inserimentO inziale dell'admin.
+CREATE TABLE utenti
+(
     id SERIAL PRIMARY KEY NOT NULL,
     nome TEXT NOT NULL,
     cognome TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    data_creazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    data_creazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ruolo INT REFERENCES ruoli(id) NOT NULL
 );
 
-CREATE TABLE ruoli(
-    id SERIAL PRIMARY KEY NOT NULL,
-    nome TEXT NOT NULL UNIQUE
-);
+INSERT INTO utenti
+    (nome, cognome, email, password, ruolo)
+VALUES
+    ('Mario', 'Rossi', 'mario.rossi@gmail.com', 'password123', 1);
 
-CREATE TABLE permessi(
-    id SERIAL PRIMARY KEY NOT NULL,
-    nome TEXT NOT NULL UNIQUE
-);
 
-CREATE TABLE ruoli_permessi(
-    idPermesso INTEGER REFERENCES permessi(id),
-    idRuolo INTEGER REFERENCES ruoli(id),
-    permission_type CHAR 
-    PRIMARY KEY (idRuolo, idPermesso)
-);
 
-CREATE TABLE indirizzo (
+-- TO DO BETTER
+CREATE TABLE indirizzo
+(
     id SERIAL PRIMARY KEY NOT NULL,
     idUtente INTEGER REFERENCES utenti(id),
     via TEXT NOT NULL,
@@ -35,29 +88,34 @@ CREATE TABLE indirizzo (
     nazione TEXT NOT NULL
 );
 
-CREATE TABLE prodotti (
+CREATE TABLE prodotti
+(
     id SERIAL PRIMARY KEY NOT NULL,
     nome TEXT NOT NULL,
-    foto TEXT NOT NULL,  -- corretto IMG -> TEXT
+    foto TEXT NOT NULL,
     descrizione TEXT NOT NULL,
     prezzo DECIMAL(10, 2) NOT NULL,
     quantita INT NOT NULL,
     data_creazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE preferiti (
-  idUtente INTEGER REFERENCES utenti(id),
-  idProdotto INTEGER REFERENCES prodotti(id),
-  PRIMARY KEY (idUtente, idProdotto)
+CREATE TABLE preferiti
+(
+    idUtente INTEGER REFERENCES utenti(id),
+    idProdotto INTEGER REFERENCES prodotti(id),
+    PRIMARY KEY (idUtente, idProdotto)
 );
 
-CREATE TABLE artigiani (
+CREATE TABLE artigiani
+(
     idUtente INTEGER REFERENCES utenti(id),
-    iban TEXT NOT NULL,  -- meglio usare TEXT per evitare problemi con numeri grandi
+    iban TEXT NOT NULL,
+    -- meglio usare TEXT per evitare problemi con numeri grandi
     mestiere TEXT NOT NULL
 );
 
-CREATE TABLE carrello (
+CREATE TABLE carrello
+(
     id SERIAL PRIMARY KEY NOT NULL,
     idUtente INTEGER REFERENCES utenti(id),
     idProdotto INTEGER REFERENCES prodotti(id),
@@ -65,7 +123,8 @@ CREATE TABLE carrello (
     data_creazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE ordine (
+CREATE TABLE ordine
+(
     id SERIAL PRIMARY KEY NOT NULL,
     idUtente INTEGER REFERENCES utenti(id),
     data_ordine TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -74,7 +133,8 @@ CREATE TABLE ordine (
     indirizzo INTEGER REFERENCES indirizzo(id)
 );
 
-CREATE TABLE ordine_prodotti (
+CREATE TABLE ordine_prodotti
+(
     idOrdine INTEGER REFERENCES ordine(id),
     idProdotto INTEGER REFERENCES prodotti(id),
     quantita INTEGER NOT NULL,
