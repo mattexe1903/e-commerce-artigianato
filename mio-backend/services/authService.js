628c630f0ef34970cde69772eae9019bbbe1fd68
mattexe <teomarelli2@gmail.com>
2025-05-02 22:27:07 +0200

    cifratura password

--- mio-backend/services/authService.js
+++ mio-backend/services/authService.js
@@ -1,12 +1,12 @@
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const userModel = require('../models/userModel');
 
 const login = async (email, password) => {
   const user = await userModel.getUserByEmail(email);
   if (!user) throw new Error('Credenziali non valide');
 
-  //const validPassword = await bcrypt.compare(password, user.password);
-  const validPassword = password === user.password;
+  const validPassword = await bcrypt.compare(password, user.password);
+  //const validPassword = password === user.password;
   if (!validPassword) throw new Error('Credenziali non valide');
 
   return {
@@ -23,8 +23,8 @@ const register = async (nome, cognome, email, password, ruolo) => {
   const existingUser = await userModel.getUserByEmail(email);
   if (existingUser) throw new Error('Email già registrata');
 
-  //const hashedPassword = await bcrypt.hash(password, 10);
-  const hashedPassword = password;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  //const hashedPassword = password;
   const newUser = await userModel.createUser(nome, cognome, email, hashedPassword, ruolo);
 
   return {
