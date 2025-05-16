5931cb4901567cb7f3ec27b00b9b625aba710384
mattexe <teomarelli2@gmail.com>
2025-05-16 21:43:12 +0200

    fix configuration file

---
+++ mio-backend/createAdmin.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt');
+const db = require('./db');
+
+const createAdmin = async () => {
+  const email = 'admin@example.com';
+  const plainPassword = 'passwordSicura';
+  const hashedPassword = await bcrypt.hash(plainPassword, 12);
+  
+  await db.query(`
+    INSERT INTO users (user_name, surname, email, user_password, user_role)
+    VALUES ($1, $2, $3, $4, $5)
+  `, ['admin', 'admin', email, hashedPassword, 1]);
+
+  console.log('Admin creato con successo!');
+  process.exit();
+};
+
+createAdmin();
