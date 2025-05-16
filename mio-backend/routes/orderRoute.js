8421959317d9438df56aa093181de96bcf8fe533
mattexe <teomarelli2@gmail.com>
2025-05-16 21:25:24 +0200

    fix model

--- mio-backend/routes/orderRoute.js
+++ mio-backend/routes/orderRoute.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { protect } = require('../middleware/userMiddleware');
 
+// Route per creare un nuovo ordine
 router.post('/createOrder', protect, orderController.createOrder);
 
+// Route per ottenere tutti gli ordini di un utente
 router.get('/getOrdersByUserId', protect, orderController.getOrdersByUserId);
 
+// Route per ottenere tutti gli ordini di un artigiano
 router.get('/getOrdersByArtisanId', protect, orderController.getOrdersByArtisanId);
 
+// Route per ottenere tutti gli ordini
 router.get('/sales', orderController.getSales);
 
+// Route per ottenere gli ordini di un artigiano
 router.get('/salesByArtisanId', protect, orderController.getDailySalesByArtisan);
-
-
 
 module.exports = router;
--- mio-backend/routes/userRoute.js
+++ mio-backend/routes/userRoute.js
@@ -6,20 +6,28 @@ const { protect } = require('../middleware/userMiddleware');
 // Route restituzione informazioni utente
 router.get('/user', protect,  userController.getUserInfo);
 
+// Route per ottenere le informazioni dell'utente
 router.get('/userInfo', protect, userController.getUserInformation);
 
+// Route per ottenere le informazioni extra dell'artigiano
 router.get('/user/:id/infoartigiani', protect, userController.getArtigianiInfo);
 
+// Route per inviare la mail di reset della password
 router.post('/send-reset-email', userController.sendResetEmail);
 
+// Route per reimpostare la password
 router.post('/reset-password', userController.resetPassword);
 
+// Route per aggiungere un nuovo indirizzo ad un utente
 router.get('/addAddress', protect, userController.addUserAddress);
 
+// Route per ottenere l'inventario di un artigiano
 router.get('/getInventory', protect, userController.getInventory);
 
+// Route per ottenere tutte le informazioni di un artigiano
 router.get('/getArtisanRegistered', userController.getArtisanRegistered);
 
+// Route per la rimozionedi un artigiano
 router.delete('/artisan/:id', userController.deleteArtisan);
 
 module.exports = router;
